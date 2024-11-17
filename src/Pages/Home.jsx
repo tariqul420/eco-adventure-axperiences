ce66d2d0ce16cb168ec675894994de334acd6737
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-11-17 18:53:51 +0600

    install react helmate

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,24 @@
 import { useContext } from "react";
 import { AuthContext } from "../Context/AuthContext";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
     const { signOutUser, user } = useContext(AuthContext)
 
     return (
-        <div>
-            {
-                user ? (<button onClick={signOutUser} className="btn">Logout</button>) : (<Link to="/signIn"  
-                    className="btn">SignIn</Link>)
-            }
-        </div>
+        <>
+            <Helmet>
+                <title>Home - Project Name</title>
+            </Helmet>
+
+            <div>
+                {
+                    user ? (<button onClick={signOutUser} className="btn">Logout</button>) : (<Link to="/signIn"
+                        className="btn">SignIn</Link>)
+                }
+            </div>
+        </>
     );
 };
 
